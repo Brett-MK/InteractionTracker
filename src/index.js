78cc866fdbf0155f55f8550103af9cf1c60ddb60
Brett Knadle <brett182x@gmail.com>
2021-03-30 11:23:41 -0700

    adding firebase authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 ReactDOM.render((
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 ), document.getElementById('root'));
 
